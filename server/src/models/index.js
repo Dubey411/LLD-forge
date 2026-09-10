edfdaf10e68c2687dde927a598abc52c300bd324
Dubey411 <dubeytech9619@gmail.com>
2026-09-10 19:59:59 +0530

    feat(models): export barrel module for domain models

---
+++ server/src/models/index.js
@@ -0,0 +1,11 @@
+const Problem = require('./Problem');
+const Attempt = require('./Attempt');
+const Submission = require('./Submission');
+const Evaluation = require('./Evaluation');
+
+module.exports = {
+  Problem,
+  Attempt,
+  Submission,
+  Evaluation
+};
